53d43b6a5b92121de2242c4b4e7cfad3ba3bec1a
OisinSmith-STUDENT <OisinSmith-STUDENT@S-6K1H504-SL35>
2026-01-07 09:33:50 +0000

    Add project files.

---
+++ S00226089_OOP_2026_EXAM/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace S00226089_OOP_2026_EXAM
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
